fadf683341cb28655f90f7bc6d49cbb72c2583d0
khoabm <buiminhkhoa2706@gmail.com>
2023-12-19 11:52:47 +0700

    add isDriver to GuardianInfo

--- lib/models/guardian_model.dart
+++ lib/models/guardian_model.dart
@@ -1,11 +1,13 @@
 import 'package:goshare/models/user_profile_model.dart';
 
 class GuardianInfo extends UserProfileModel {
-  GuardianInfo(
-      {required super.id,
-      required super.name,
-      required super.phone,
-      required super.gender,
-      required super.birth,
-      super.avatarUrl});
+  GuardianInfo({
+    required super.id,
+    required super.name,
+    required super.phone,
+    required super.gender,
+    required super.birth,
+    super.avatarUrl,
+    required super.isDriver,
+  });
 }
